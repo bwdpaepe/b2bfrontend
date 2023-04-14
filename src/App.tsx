d934ba799debfcc4415141c5d5880b5a2557bcd4
JoachimDauchot <dauchot.joachim@gmail.com>
2023-04-14 15:58:28 +0200

    user context globaal gemaakt

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {Grid, GridItem} from "@chakra-ui/react";
+import { Grid, GridItem } from "@chakra-ui/react";
 import MenuBar from "./components/subComponents/MenuBar";
 import Footer from "./components/subComponents/Footer";
 import { Route, Routes } from "react-router";
@@ -9,38 +9,38 @@ import Notificaties from "./components/mainPages/Notificaties";
 import NotFound from "./components/mainPages/NotFound";
 import Winkelmand from "./components/mainPages/Winkelmand";
 
-
 export const UserContext = React.createContext("");
 
 function App() {
-
-
-
   return (
     <>
-      <Grid
-        templateAreas={`"menu""content""footer"`}
-        h="100vh"
-        templateRows="auto 1fr auto"
+      <UserContext.Provider
+        value={
+          localStorage.getItem("User") ? localStorage.getItem("User")! : ""
+        }
       >
-        <GridItem gridArea="menu">
-          <UserContext.Provider value={localStorage.getItem("User")? localStorage.getItem("User")! : ""}>
-          <MenuBar />
-          </UserContext.Provider>
-        </GridItem>
-        <GridItem gridArea="content">
-          <Routes>
-          <Route index element={<Home />} />
-          <Route path="/bestellingen" element={<Bestellingen />} />
-          <Route path="/notificaties" element={<Notificaties />} />
-          <Route path="/winkelmand" element={<Winkelmand />} />
-          <Route path="*" element={<NotFound />} />
-          </Routes>
-        </GridItem>
-        <GridItem gridArea="footer">
-          <Footer />
-        </GridItem>
-      </Grid>
+        <Grid
+          templateAreas={`"menu""content""footer"`}
+          h="100vh"
+          templateRows="auto 1fr auto"
+        >
+          <GridItem gridArea="menu">
+            <MenuBar />
+          </GridItem>
+          <GridItem gridArea="content">
+            <Routes>
+              <Route index element={<Home />} />
+              <Route path="/bestellingen" element={<Bestellingen />} />
+              <Route path="/notificaties" element={<Notificaties />} />
+              <Route path="/winkelmand" element={<Winkelmand />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </GridItem>
+          <GridItem gridArea="footer">
+            <Footer />
+          </GridItem>
+        </Grid>
+      </UserContext.Provider>
     </>
   );
 }
