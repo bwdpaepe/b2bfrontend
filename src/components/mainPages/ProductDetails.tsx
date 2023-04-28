c1d635c5e8598661ac5ceabd5ecf054a31e5e319
Ian Daelman <ian.daelman@student.hogent.be>
2023-04-28 11:41:18 +0200

    connectie met backend ok

--- src/components/mainPages/ProductDetails.tsx
+++ src/components/mainPages/ProductDetails.tsx
@@ -1,17 +1,31 @@
 import { useNavigate } from "react-router";
 import { useParams } from "react-router-dom";
 import { Text } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+import Product from "../../type/Product";
+import { productenByProductId } from "../../service/producten";
 
 export default function ProductDetails() {
-  const { bedrijfIdString } = useParams();
+  const { bedrijfIdString, productIdString } = useParams();
   const navigate = useNavigate();
   function handleNavigate(pathname: string) {
     navigate(pathname);
   }
 
+  const [product, setProduct] = useState<Product>();
+
+  useEffect(() => {
+    async function fetchProduct() {
+      const productData = await productenByProductId(Number(productIdString));
+      setProduct(productData);
+    }
+    fetchProduct();
+  }, [productIdString]);
+
+  console.log(JSON.stringify(product));
   return (
     <div>
-      <h1>ProductDetails</h1>
+      <h1>ProductDetails van {product?.naam}</h1>
       <Text
         color="red"
         onClick={() => handleNavigate(`/producten/${bedrijfIdString}`)}
--- src/service/producten.ts
+++ src/service/producten.ts
@@ -1,10 +1,23 @@
 import http from "./http";
 
-export async function productenByBedrijfId(id: number) {
+export async function productenByBedrijfId(bedrijfId: number) {
   try {
-    const response = await http.get(`/products/bedrijfId?bedrijfId=${id}`);
+    const response = await http.get(
+      `/products/bedrijfId?bedrijfId=${bedrijfId}`
+    );
     return response.data;
   } catch (error: any) {
-    throw Error("er is iets misgegaan");
+    throw new Error("er is iets misgegaan");
+  }
+}
+
+export async function productenByProductId(productId: number) {
+  try {
+    const response = await http.get(
+      `/products/productId?productId=${productId}`
+    );
+    return response.data;
+  } catch (error: any) {
+    throw new Error("er is iets misgegaan");
   }
 }
