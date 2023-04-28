1387a961284e03d285795c09a094e5c15853d726
Ian Daelman <ian.daelman@student.hogent.be>
2023-04-28 20:46:16 +0200

    detail pagina WIP

--- src/components/mainPages/ProductDetails.tsx
+++ src/components/mainPages/ProductDetails.tsx
@@ -22,6 +22,7 @@ import {
   NumberInputStepper,
   NumberIncrementStepper,
   NumberDecrementStepper,
+  Divider,
 } from "@chakra-ui/react";
 import { AiOutlineShoppingCart } from "react-icons/ai";
 import { MdLocalShipping } from "react-icons/md";
@@ -43,124 +44,123 @@ export default function ProductDetails() {
     fetchProduct();
   }, [productIdString]);
   return (
-    <Container maxW={"7xl"}>
-      <SimpleGrid
-        columns={{ base: 1, lg: 2 }}
-        spacing={{ base: 8, md: 10 }}
-        py={{ base: 18, md: 24 }}
-      >
-        <Flex justifyContent="center">
+    <Flex>
+      <Container maxW={"5xl"}>
+        <SimpleGrid
+          columns={{ base: 1, lg: 2 }}
+          spacing={{ base: 4, md: 10 }}
+          py={{ base: 10, md: 24 }}
+        >
+          <Flex justifyContent={{ base: "space-between", sm: "center" }}>
+            <Stack spacing={{ base: 6, md: 10 }}>
+              {product && (
+                <Image
+                  rounded={"md"}
+                  alt={"product image"}
+                  src={require(`../../assets/productafbeeldingen/${bedrijfIdString}/${product?.pictureFilename}`)}
+                  fit={"cover"}
+                  w={"100%"}
+                  h={{ base: "100%", sm: "400px", lg: "500px" }}
+                />
+              )}
+            </Stack>
+          </Flex>
           <Stack spacing={{ base: 6, md: 10 }}>
-            {product && (
-              <Image
-                rounded={"md"}
-                alt={"product image"}
-                src={require(`../../assets/productafbeeldingen/${bedrijfIdString}/${product?.pictureFilename}`)}
-                fit={"cover"}
-                align={"center"}
-                w={"100%"}
-                h={{ base: "100%", sm: "400px", lg: "500px" }}
-              />
-            )}
-          </Stack>
-        </Flex>
-        <Stack spacing={{ base: 6, md: 10 }}>
-          <Stack
-            spacing={{ base: 4, sm: 6 }}
-            direction={"column"}
-            divider={
-              <StackDivider
-                borderColor={useColorModeValue("gray.200", "gray.600")}
-              />
-            }
-          >
-            <VStack spacing={{ base: 4, sm: 6 }}>
-              <Text
-                lineHeight={1.1}
-                fontWeight={600}
-                fontSize={{ base: "2xl", sm: "4xl", lg: "5xl" }}
-              >
-                {product?.naam}
-              </Text>
-              <Text
-                color={useColorModeValue("gray.500", "gray.400")}
-                fontSize={"2xl"}
-                fontWeight={"300"}
-              >
-                {product?.omschrijving} met product ID: {product?.productId} en
-                heeft als voorraad: {product?.voorraad}. Aangeboden door bedrijf
-                met ID: {bedrijfIdString}.
-              </Text>
-            </VStack>
+            <Stack spacing={{ base: 4, sm: 6 }} direction={"column"}>
+              <VStack spacing={{ base: 4, sm: 6 }}>
+                <Text
+                  lineHeight={1.1}
+                  fontWeight={600}
+                  fontSize={{ base: "xl", sm: "2xl", lg: "3xl" }}
+                >
+                  {product?.naam}
+                </Text>
+                <Text
+                  color={useColorModeValue("gray.500", "gray.400")}
+                  fontSize={"xl"}
+                  fontWeight={"300"}
+                >
+                  {product?.omschrijving} met product ID: {product?.productId}{" "}
+                  en heeft als voorraad: {product?.voorraad}. Aangeboden door
+                  bedrijf met ID: {bedrijfIdString}.
+                </Text>
+              </VStack>
 
-            <Box>
-              <Text
-                fontSize={{ base: "16px", lg: "18px" }}
-                color={useColorModeValue("yellow.500", "yellow.300")}
-                fontWeight={"500"}
-                textTransform={"uppercase"}
-                mb={"4"}
-              >
-                Product Details
-              </Text>
+              <Box>
+                <Text
+                  fontSize={{ base: "16px", lg: "18px" }}
+                  color={useColorModeValue("yellow.500", "yellow.300")}
+                  fontWeight={"500"}
+                  textTransform={"uppercase"}
+                  mb={"4"}
+                >
+                  Product Details
+                </Text>
 
-              <List spacing={2}>
-                <ListItem>
-                  <Text as={"span"} fontWeight={"bold"}>
-                    Between lugs:
-                  </Text>{" "}
-                  20 mm
-                </ListItem>
-                <ListItem>
-                  <Stack direction="row" alignItems="center">
-                    <MdLocalShipping />
-                    <Text>2-3 business days delivery</Text>
-                  </Stack>
-                </ListItem>
-                <ListItem>
-                  <Text
-                    color="red"
-                    onClick={() =>
-                      handleNavigate(`/producten/${bedrijfIdString}`)
-                    }
-                    _hover={{
-                      cursor: "pointer",
-                      textDecoration: "underline",
-                    }}
-                  >{`< Terug`}</Text>
-                </ListItem>
-              </List>
-            </Box>
+                <List spacing={2}>
+                  <ListItem>
+                    <Text as={"span"} fontWeight={"bold"}>
+                      Prijs:
+                    </Text>{" "}
+                    €{product?.eenheidsprijs}
+                  </ListItem>
+                  <Divider orientation="horizontal" />
+                  <ListItem>
+                    <Text as={"span"} fontWeight={"bold"}>
+                      Levertermijn:
+                    </Text>{" "}
+                    2-3 werkdagen
+                  </ListItem>
+                  <Divider orientation="horizontal" />
+                  <ListItem>
+                    <Text as={"span"} fontWeight={"bold"}>
+                      Voorraad:
+                    </Text>{" "}
+                    {product?.voorraad}
+                  </ListItem>
+                </List>
+              </Box>
+            </Stack>
+            <Stack direction="row" alignItems="center">
+              <NumberInput
+                defaultValue={0}
+                min={0}
+                max={product?.voorraad}
+                variant="filled"
+                focusBorderColor="#ec4842"
+              >
+                <NumberInputField />
+                <NumberInputStepper>
+                  <NumberIncrementStepper />
+                  <NumberDecrementStepper />
+                </NumberInputStepper>
+              </NumberInput>
+              <Button
+                variant="solid"
+                className="button"
+                mt={4}
+                py={"4"}
+                w={"full"}
+                _hover={{
+                  transform: "translateY(2px)",
+                  boxShadow: "lg",
+                }}
+              >
+                <AiOutlineShoppingCart size="2.5rem" />
+              </Button>
+            </Stack>
+            <Text
+              color="#ec4842"
+              mt={4}
+              onClick={() => handleNavigate(`/producten/${bedrijfIdString}`)}
+              _hover={{
+                cursor: "pointer",
+                textDecoration: "underline",
+              }}
+            >{`< Terug naar productoverzicht`}</Text>
           </Stack>
-          <NumberInput
-            defaultValue={0}
-            min={0}
-            max={product?.voorraad}
-            variant="filled"
-            focusBorderColor="#ec4842"
-          >
-            <NumberInputField />
-            <NumberInputStepper>
-              <NumberIncrementStepper />
-              <NumberDecrementStepper />
-            </NumberInputStepper>
-          </NumberInput>
-          <Button
-            variant="solid"
-            className="button"
-            w={"full"}
-            mt={8}
-            size={"lg"}
-            py={"7"}
-            _hover={{
-              transform: "translateY(2px)",
-              boxShadow: "lg",
-            }}
-          >
-            <AiOutlineShoppingCart size="2.5rem" />
-          </Button>
-        </Stack>
-      </SimpleGrid>
-    </Container>
+        </SimpleGrid>
+      </Container>
+    </Flex>
   );
 }
