335fc95edb98bc59a518ec3a5b45b35aba1094b2
Bart De Paepe <bart.depaepe@student.hogent.be>
2023-05-02 23:47:57 +0200

    setup track and trace page

--- src/components/mainPages/BestellingTrackAndTrace.tsx
+++ src/components/mainPages/BestellingTrackAndTrace.tsx
@@ -1,7 +1,44 @@
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
 import { Container } from '@chakra-ui/react';
+import { SimpleGrid } from "@chakra-ui/react";
+import { Text } from "@chakra-ui/react";
+
+import Bestelling from "../../type/Bestelling";
+import { bestellingTrackAndTraceByBestellingId } from "../../service/bestellingen";
+import { BestellingStatus } from "../../enums/BestellingStatusEnum";
+import StatusCard from "../subComponents/StatusCard";
+
 export default function BestellingTrackAndTrace() {
+  const { bestellingIdString } = useParams();
+  const [bestelling, setBestelling] = useState<Bestelling>();
+  useEffect(() => {
+    async function fetchBestelling() {
+      const bestellingData = await bestellingTrackAndTraceByBestellingId(Number(bestellingIdString));
+      setBestelling(bestellingData);
+      console.log(bestellingData);
+    }
+    fetchBestelling();
+  }, [bestellingIdString]);
+  if (!bestelling) {
+    return <Text>No products found.</Text>;
+  }
   return(
     <Container maxW="70%" centerContent>
+      <SimpleGrid
+      templateColumns={{
+        base: "repeat(5, minmax(0, 1fr))",
+      }}
+      gap={6}
+      p={3}
+    >
+      {Object.keys(BestellingStatus).map((status) => {
+        return (<StatusCard
+        key={status}
+        bestelling={bestelling}
+      />);
+      })};
+    </SimpleGrid>
     </Container>
   );
 }
--- src/service/bestellingen.tsx
+++ src/service/bestellingen.tsx
@@ -27,3 +27,17 @@ export async function bestellingByBestellingId(bestellingId: number){
     throw Error("Kon de bestelling niet ophalen._");
   }
 }
+
+export async function bestellingTrackAndTraceByBestellingId(bestellingId: number){
+  try {
+    const response = await http.get(`bestellingen/${bestellingId}/track-and-trace`, {headers: {Authorization: "Bearer " + localStorage.getItem("Token")}});
+    if(response.data) {
+      return response.data;
+    }
+    else {
+      throw Error("Kon de bestelling niet ophalen.");
+    }
+  } catch (error: any) {
+    throw Error("Kon de bestelling niet ophalen._");
+  }
+}
