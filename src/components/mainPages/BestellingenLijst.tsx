053dcf99f29a267d7cd7ae108588a5d5551a64de
Ian Daelman <ian.daelman@student.hogent.be>
2023-05-17 16:15:17 +0200

    filters fix

--- src/components/mainPages/BestellingenLijst.tsx
+++ src/components/mainPages/BestellingenLijst.tsx
@@ -9,15 +9,12 @@ import { Container } from "@chakra-ui/react";
 import { Heading } from "@chakra-ui/react";
 import { SearchIcon } from "@chakra-ui/icons";
 import { useEffect, useMemo, useState } from "react";
-import { useNavigate } from "react-router";
 import Bestelling from "../../type/Bestelling";
 //import Bestelling from "./Bestelling";
 import { DataTable } from "../subComponents/DataTable";
 import { bestellingenByAankoper } from "../../service/bestellingen";
 import "../../styling/bestellingen.css";
 import { BestellingStatus } from "../../enums/BestellingStatusEnum";
-import enumKeys from "../../util/Util";
-import OrderAdresChangePopover from "../subComponents/bestelling/OrderAdresChangePopover";
 
 type UnitConversion = {
   orderId: string;
@@ -25,7 +22,6 @@ type UnitConversion = {
   datumGeplaatst: Date;
   status: BestellingStatus;
   details: JSX.Element;
-  wijzigen: JSX.Element;
 };
 
 const columnHelper = createColumnHelper<UnitConversion>();
@@ -51,10 +47,6 @@ const columns = [
     cell: (info) => info.getValue(),
     header: "Details",
   }),
-  columnHelper.accessor("wijzigen", {
-    cell: (info) => info.getValue(),
-    header: "Wijzigen",
-  }),
 ];
 
 export default function BestellingenLijst() {
@@ -65,11 +57,6 @@ export default function BestellingenLijst() {
   const [searchDatum, setSearchDatum] = useState("");
   const [textStatus, setTextStatus] = useState("");
   const [searchStatus, setSearchStatus] = useState("");
-
-  const navigate = useNavigate();
-  function handleNavigate(pathname: string) {
-    navigate(pathname);
-  }
 
   useEffect(() => {
     async function fetchBestellingen() {
@@ -83,22 +70,7 @@ export default function BestellingenLijst() {
     return {
       ...bestelling,
       email: bestelling.aankoper.email,
-      details: (
-        <Button
-          colorScheme="white"
-          onClick={() =>
-            handleNavigate(`/bestellingen/${bestelling.bestellingId}`)
-          }
-        >
-          Zie details
-        </Button>
-      ),
-      wijzigen:
-        bestelling.status.toString() === "GEPLAATST" ? ( //TODO: contoleren waarom dit niet werkt bestelling.status === BestellingStatus.GEPLAATST ? ( ... ) : ( ... )
-          <OrderAdresChangePopover bestelling={bestelling} />
-        ) : (
-          <Text>Niet wijzigbaar</Text>
-        ),
+      details: <Button colorScheme="white">Zie details</Button>,
     };
   });
 
@@ -111,10 +83,10 @@ export default function BestellingenLijst() {
           b.aankoper.email
             .toLowerCase()
             .includes(searchAankoper.toLowerCase()) &&
-          (searchDatum === "" ||
+          (searchDatum == "" ||
             new Date(b.datumGeplaatst).getTime() ===
               new Date(searchDatum).getTime()) &&
-          (searchStatus === "" || BestellingStatus[b.status] === searchStatus)
+          (searchStatus == "" || BestellingStatus[b.status] == searchStatus)
         );
       }),
     [bestellingen, searchAankoper, searchDatum, searchStatus]
@@ -153,13 +125,24 @@ export default function BestellingenLijst() {
             placeholder="Selecteer status"
             onChange={(e) => setTextStatus(e.target.value)}
           >
-            {enumKeys(BestellingStatus).map((status: string) => {
-              return (
-                <option key={status} value={BestellingStatus[parseInt(status)]}>
-                  {BestellingStatus[parseInt(status)]}
-                </option>
-              );
-            })}
+            {
+              //Object.values(BestellingStatus).map((value: string, index: number, array: string[]) => {return (<option></option>);})
+            }
+            <option key={1} value={BestellingStatus.GEPLAATST}>
+              GEPLAATST
+            </option>
+            <option key={2} value={BestellingStatus.VERWERKT}>
+              VERWERKT
+            </option>
+            <option key={3} value={BestellingStatus.VERZONDEN}>
+              VERZONDEN
+            </option>
+            <option key={4} value={BestellingStatus.UIT_VOOR_LEVERING}>
+              UIT VOOR LEVERING
+            </option>
+            <option key={5} value={BestellingStatus.GELEVERD}>
+              GELEVERD
+            </option>
           </Select>
         </GridItem>
         <GridItem w="100%" h="10" className="gridItem">
