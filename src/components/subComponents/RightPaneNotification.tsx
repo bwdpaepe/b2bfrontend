5c74912e53937a2d535251273512de3f8f78dcdb
JoachimDauchot <dauchot.joachim@gmail.com>
2023-05-18 09:08:36 +0200

    styling

--- src/components/subComponents/RightPaneNotification.tsx
+++ src/components/subComponents/RightPaneNotification.tsx
@@ -1,4 +1,4 @@
-import { Box, Grid, GridItem } from "@chakra-ui/react";
+import { Box, Grid, GridItem, Text } from "@chakra-ui/react";
 import Notifications from "../../type/Notifications";
 
 
@@ -11,10 +11,10 @@ export default function RightPaneNotification(props: {notification : Notificatio
     
     {props.notification? <Box id="rightPaneNotification" display="block" mt="100px">
         <Grid id="notificatieGrid" padding="5px" pt="10%" gap="10px">
-            <GridItem>Je bestellings nummer: {props.notification.bestellingId}</GridItem>
-            <GridItem>Track and Trace code: {props.notification.trackAndTraceCode? props.notification.trackAndTraceCode : "Nog geen code toegewezen"}</GridItem>
-            <GridItem>Bestelling Status: {props.notification.bestellingStatus}</GridItem>
-            <GridItem>Ontvangen op: {props.notification.creationDate.slice(0,10)}</GridItem>
+            <GridItem><Text fontWeight="bold" color="red">Bestelling nummer </Text> {props.notification.bestellingId}</GridItem>
+            <GridItem><Text fontWeight="bold" color="red">Track and Trace code </Text> {props.notification.trackAndTraceCode? props.notification.trackAndTraceCode : "Nog geen code toegewezen"}</GridItem>
+            <GridItem><Text fontWeight="bold" color="red">Bestelling Status: </Text>{props.notification.bestellingStatus}</GridItem>
+            <GridItem><Text fontWeight="bold" color="red">Geplaatst op:  </Text> {props.notification.creationDate.slice(0,10)}</GridItem>
         </Grid>
 
     </Box> : <></>}
