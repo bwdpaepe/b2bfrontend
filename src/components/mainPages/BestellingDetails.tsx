1afc71c922d8e17a167126fee329b2702bf7222e
DIMITRI VALCKENIER <dimitri.valckenier@student.hogent.be>
2023-05-21 11:25:38 +0200

    Fix crash bestellingDetails.tsx

--- src/components/mainPages/BestellingDetails.tsx
+++ src/components/mainPages/BestellingDetails.tsx
@@ -1,50 +1,51 @@
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
-import { Container } from '@chakra-ui/react';
-import { Flex, Spacer } from '@chakra-ui/react';
-import { Heading, Box, Text} from "@chakra-ui/react";
-import { Link } from '@chakra-ui/react';
+import { Container } from "@chakra-ui/react";
+import { Flex, Spacer } from "@chakra-ui/react";
+import { Heading, Box, Text } from "@chakra-ui/react";
+import { Link } from "@chakra-ui/react";
 import { useNavigate } from "react-router";
 
 import BestellingDetail from "../../type/BestellingDetail";
 import BesteldProductCard from "../subComponents/BesteldProductCard";
 import { bestellingByBestellingId } from "../../service/bestellingen";
-import "../../styling/bestellingen.css"
+import "../../styling/bestellingen.css";
 import BestellingByTrackAndTrace from "../../type/BestellingByTrackAndTrace";
 
 export default function BestellingDetails() {
   const { bestellingIdString } = useParams();
   const [bestelling, setBestelling] = useState<BestellingDetail>();
-  const [bestellingTTC, setBestellingTTC] = useState<BestellingByTrackAndTrace>();
+  const [bestellingTTC, setBestellingTTC] =
+    useState<BestellingByTrackAndTrace>();
 
   const navigate = useNavigate();
   function handleNavigate(pathname: string) {
     navigate(pathname);
   }
-    function handleNavigateWithParams(pathname: string, bTTC: BestellingByTrackAndTrace|undefined) {
-      navigate(pathname,{state:bTTC});
-    }
+  function handleNavigateWithParams(
+    pathname: string,
+    bTTC: BestellingByTrackAndTrace | undefined
+  ) {
+    navigate(pathname, { state: bTTC });
+  }
 
-    useEffect(() => {
+  useEffect(() => {
     async function fetchBestelling() {
-      const bestellingData = await bestellingByBestellingId(Number(bestellingIdString));
-      if(bestellingData) {
+      const bestellingData = await bestellingByBestellingId(
+        Number(bestellingIdString)
+      );
+      if (bestellingData) {
         setBestelling(bestellingData);
-        
-        
-      }
-      else {
-        throw Error (`kon bestelling met id ${bestellingIdString} niet ophalen`);
+      } else {
+        throw Error(`kon bestelling met id ${bestellingIdString} niet ophalen`);
       }
     }
     fetchBestelling();
-    
   }, [bestellingIdString]);
 
   useEffect(() => {
-    
-    if(bestelling) {
-      const bTTC:BestellingByTrackAndTrace = {
+    if (bestelling) {
+      const bTTC: BestellingByTrackAndTrace = {
         bestellingId: bestelling?.bestellingId,
         status: bestelling?.status,
         datumGeplaatst: bestelling?.datumGeplaatst,
@@ -52,18 +53,23 @@ export default function BestellingDetails() {
         orderId: bestelling?.orderId,
         trackAndTraceCode: bestelling?.trackAndTraceCode,
         transportdienst: bestelling?.transportdienst,
-        notification: bestelling?.notification
+        notification: bestelling?.notification,
       };
       setBestellingTTC(bTTC);
     }
   }, [bestelling]);
 
-  return(
+  return (
     <Container maxW="70%" centerContent>
-      <Text onClick={() => handleNavigate("/bestellingen")} _hover={{
-                cursor: "pointer",
-                textDecoration: "underline",
-              }}>Terug naar overzicht</Text>
+      <Text
+        onClick={() => handleNavigate("/bestellingen")}
+        _hover={{
+          cursor: "pointer",
+          textDecoration: "underline",
+        }}
+      >
+        Terug naar overzicht
+      </Text>
       <Heading>Aankoop {bestelling?.orderId}</Heading>
       <Flex direction={"column"} className={"bestellingOverzicht"}>
         <Box id="BesteldProductCardHolder">
@@ -86,73 +92,50 @@ export default function BestellingDetails() {
         </Box>
         <Flex className={"bestellingDetails"}>
           <Flex direction={"column"}>
-          <Text fontWeight="bold">
-            Order Id
-          </Text>
-          <Text>
-            {bestelling?.orderId}
-          </Text>
-          <Text fontWeight="bold">
-            Besteld op
-          </Text>
-          <Text>
-            {bestelling?.datumGeplaatst.toString()}
-          </Text>
-          <Text fontWeight="bold">
-            Aankoopstatus
-          </Text>
-          <Text>
-            {bestelling?.status}
-          </Text>
-          </Flex>  
+            <Text fontWeight="bold">Order Id</Text>
+            <Text>{bestelling?.orderId}</Text>
+            <Text fontWeight="bold">Besteld op</Text>
+            <Text>{bestelling?.datumGeplaatst.toString()}</Text>
+            <Text fontWeight="bold">Aankoopstatus</Text>
+            <Text>{bestelling?.status}</Text>
+          </Flex>
           <Spacer />
           <Flex direction={"column"}>
-          <Text fontWeight="bold">
-            Leveradres
-          </Text>
-          <Text>
-            {bestelling?.leveradresStraat} {bestelling?.leveradresNummer}
-          </Text>
-          <Text>
-            {bestelling?.leveradresPostcode} {bestelling?.leveradresStad}
-          </Text>
-          <Text>
-            {bestelling?.leveradresLand}
-          </Text>
-          <Text fontWeight="bold">
-            Track en trace code
-          </Text>
-          {bestelling?.trackAndTraceCode ? <Link colorScheme='red' onClick={() => handleNavigateWithParams(`/track-and-trace`, bestellingTTC)} _hover={{
-                cursor: "pointer",
-                textDecoration: "underline",
-              }}>
-            {bestelling?.trackAndTraceCode}
-          </Link> : <Text>
-            Track and Trace niet beschikbaar
-          </Text>}
-          
-          </Flex>  
+            <Text fontWeight="bold">Leveradres</Text>
+            <Text>
+              {bestelling?.leveradresStraat} {bestelling?.leveradresNummer}
+            </Text>
+            <Text>
+              {bestelling?.leveradresPostcode} {bestelling?.leveradresStad}
+            </Text>
+            <Text>{bestelling?.leveradresLand}</Text>
+            <Text fontWeight="bold">Track en trace code</Text>
+            {bestelling?.trackAndTraceCode ? (
+              <Link
+                colorScheme="red"
+                onClick={() =>
+                  handleNavigateWithParams(`/track-and-trace`, bestellingTTC)
+                }
+                _hover={{
+                  cursor: "pointer",
+                  textDecoration: "underline",
+                }}
+              >
+                {bestelling?.trackAndTraceCode}
+              </Link>
+            ) : (
+              <Text>Track and Trace niet beschikbaar</Text>
+            )}
+          </Flex>
           <Spacer />
           <Flex direction={"column"}>
-          <Text fontWeight="bold">
-            Verpakking
-          </Text>
-          <Text>
-            {bestelling?.doos.naam}
-          </Text>
-          <Text fontWeight="bold">
-            Leverancier
-          </Text>
-          <Text>
-            {bestelling?.leverancierBedrijf.naam}
-          </Text>
-          <Text fontWeight="bold">
-            Kostenoverzicht
-          </Text>
-          <Text>
-            {bestelling?.totalPrice.toFixed(2)} €
-          </Text>
-          </Flex>  
+            <Text fontWeight="bold">Verpakking</Text>
+            <Text>{bestelling?.doos.naam}</Text>
+            <Text fontWeight="bold">Leverancier</Text>
+            <Text>{bestelling?.leverancierBedrijf.naam}</Text>
+            <Text fontWeight="bold">Kostenoverzicht</Text>
+            <Text>{bestelling?.totalPrice.toFixed(2)} €</Text>
+          </Flex>
         </Flex>
       </Flex>
     </Container>
--- src/components/subComponents/BesteldProductCard.tsx
+++ src/components/subComponents/BesteldProductCard.tsx
@@ -10,46 +10,59 @@ export default function BesteldProductCard(props: {
   subtotal: number | null;
 }) {
 
-  console.log(`../../assets/productafbeeldingen/${props.bedrijfsId}/${props.pictureFilename}`);
+  let imageUrl;
+  try {
+    imageUrl = require(`../../assets/productafbeeldingen/${props.bedrijfsId}/${props.pictureFilename}`);
+  } catch {
+    imageUrl = require("../../assets/missing-image.jpg");
+  }
+
   return (
     <>
       <Box className="BesteldProductCard">
-      <Grid className="BesteldProductGrid" alignItems="top">
+        <Grid className="BesteldProductGrid" alignItems="top">
+          <GridItem className={"box1"}>
+            <Box>
+              {" "}
+              <Image
+                rounded={"md"}
+                alt={"product image"}
+                src={imageUrl}
+                fit={"cover"}
+                w={"100%"}
+              />{" "}
+            </Box>
+          </GridItem>
+          <GridItem className={"box2"}>
+            <Box h={"100%"}>
+              <Flex
+                direction={"column"}
+                h={"100%"}
+                className={"flex-space-between"}
+              >
+                <Text fontWeight="bold">Naam product</Text>
+                <Text>{props.naam}</Text>
+                <Text fontWeight="bold">Aantal x eenheidsprijs</Text>
+                <Text>
+                  {props.aantal} x {props.eenheidsprijs?.toFixed(2)} €
+                </Text>
+                <Text></Text>
+                <Text></Text>
+                <Text></Text>
+                <Text fontWeight="bold">Subtotaal</Text>
+                <Text>{props.subtotal?.toFixed(2)} €</Text>
+              </Flex>
+            </Box>
+          </GridItem>
 
-<GridItem className={"box1"}><Box> <Image
-                  rounded={"md"}
-                  alt={"product image"}
-                  src={require(`../../assets/productafbeeldingen/${props.bedrijfsId}/${props.pictureFilename}`)}
-                  fit={"cover"}
-                  w={"100%"}
-                /> </Box></GridItem>
-<GridItem className={"box2"}>
-  <Box h={'100%'}>
-    <Flex direction={'column'} h={'100%'} className={"flex-space-between"}>
-    <Text fontWeight="bold">
-            Naam product
-          </Text>
-      <Text>{props.naam}</Text>
-      <Text fontWeight="bold">
-            Aantal x eenheidsprijs
-          </Text>
-      <Text>{props.aantal} x {props.eenheidsprijs?.toFixed(2)} €</Text>
-      <Text></Text>
-      <Text></Text>
-      <Text></Text>
-      <Text fontWeight="bold">
-            Subtotaal
-          </Text>
-      <Text>{props.subtotal?.toFixed(2)} €</Text>
-    </Flex>
-    </Box>
-</GridItem>
-
-<GridItem className={"box3"}><Box><Text fontWeight="bold">
-            Omschrijving
-          </Text><Text>{props.omschrijving}</Text></Box></GridItem>
-
-</Grid>
+          <GridItem className={"box3"}>
+            <Box>
+              <Text fontWeight="bold">Omschrijving</Text>
+              <Text>{props.omschrijving}</Text>
+            </Box>
+          </GridItem>
+        </Grid>
       </Box>
-    </>);
+    </>
+  );
 }
