ec5a2bb0851b17e5a46899b8bce6c267064719fc
DIMITRI VALCKENIER <dimitri.valckenier@student.hogent.be>
2023-05-21 09:14:54 +0200

    correctie dependency-array in BestellingTrackAndTrace

--- src/components/mainPages/BestellingTrackAndTrace.tsx
+++ src/components/mainPages/BestellingTrackAndTrace.tsx
@@ -1,50 +1,54 @@
-import {useEffect, useState} from  "react";
-import {useLocation} from 'react-router-dom';
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
 
-import { Container, Text, Heading } from '@chakra-ui/react';
+import { Container, Text, Heading } from "@chakra-ui/react";
 
 import "../../styling/trackAndTrace.css";
 import BestellingByTrackAndTrace from "../../type/BestellingByTrackAndTrace";
-import TrackAndTraceFormulier from '../subComponents/TrackAndTraceFormulier';
-import TrackAndTraceResultaat from '../subComponents/TrackAndTraceResultaat';
+import TrackAndTraceFormulier from "../subComponents/TrackAndTraceFormulier";
+import TrackAndTraceResultaat from "../subComponents/TrackAndTraceResultaat";
 
 export default function BestellingTrackAndTrace() {
   const [bestelling, setBestelling] = useState<BestellingByTrackAndTrace>();
   const location = useLocation();
 
   useEffect(() => {
-    if(location.state ){
+    if (location.state) {
       setBestelling(location.state);
+    } else {
+      setBestelling(undefined);
     }
-    else{
-    setBestelling(undefined); 
-    }
-  }, []);
+  }, [location.state]);
 
   const handleBackToForm = () => {
     //location.state = undefined;
     setBestelling(undefined);
-    //window.location.reload(); // reload the page to set the state to undefined  
+    //window.location.reload(); // reload the page to set the state to undefined
   };
 
   if (bestelling === undefined) {
-  return(
-    <Container maxW="70%" centerContent>
-      <Heading>Track & trace gegevens</Heading>
-      <TrackAndTraceFormulier setBestelling={setBestelling} />
-    </Container>
-    
-  );
-  }
-  else {
-    return(
+    return (
       <Container maxW="70%" centerContent>
-        <Text onClick={handleBackToForm} _hover={{
-                cursor: "pointer",
-                textDecoration: "underline",
-              }}>Terug naar formulier</Text>
-        <Heading>Track & trace gegevens van aankoop {bestelling.orderId}</Heading>
-        <TrackAndTraceResultaat bestelling={bestelling}/>
+        <Heading>Track & trace gegevens</Heading>
+        <TrackAndTraceFormulier setBestelling={setBestelling} />
+      </Container>
+    );
+  } else {
+    return (
+      <Container maxW="70%" centerContent>
+        <Text
+          onClick={handleBackToForm}
+          _hover={{
+            cursor: "pointer",
+            textDecoration: "underline",
+          }}
+        >
+          Terug naar formulier
+        </Text>
+        <Heading>
+          Track & trace gegevens van aankoop {bestelling.orderId}
+        </Heading>
+        <TrackAndTraceResultaat bestelling={bestelling} />
       </Container>
     );
   }
